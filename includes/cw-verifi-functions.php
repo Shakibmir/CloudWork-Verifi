873ae749dfbfe296229f30c0cabf0aabb262c62f
Chris Kelley <chris@organicbeemedia.com>
2013-01-27 10:56:05 -0600

    added function to get user data from purchases code

--- includes/cw-verifi-functions.php
+++ includes/cw-verifi-functions.php
@@ -4,7 +4,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * @package CloudWork Verifi
  * @subpackage cw-verifi-functions.php
- * @version 0.1.2
+ * @version 0.2
  * @author Chris Kelley <chris@organicbeemedia.com)
  * @copyright Copyright © 2013 CloudWork Themes
  * @link http://cloudworkthemes.com
@@ -15,14 +15,21 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * cw_get_user_by_meta_data
  * cw_purchase_exists 
  * cw_validate_api
+ * cw_get_purchase_data
  *
 */
 
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
 /**
 * Gets user by meta key and meta value
-* 
+* This function may be depreciated in future version as its not
+* directly used by the plugin as of 0.2 
+*
 * @thanks Tom Mcfarlin http://tommcfarlin.com
 * @since 0.1
+* @uses WP_User_Query
 * @access public
 * @param mixed $meta_key
 * @param mixed $meta_value
@@ -51,14 +58,28 @@ function cw_get_user_by_meta_data( $meta_key, $meta_value ) {
 * Checks to see purchase already exists
 * 
 * @since 0.1
-* @uses cw_get_user_by_meta
+* @uses WP_User_Query
 * @access public
 * @param mixed $cw_purcahse_code
 * @return bool
 */
 function cw_purchase_exists( $input ) {
 
-	if ( $user = cw_get_user_by_meta_data('_cw_purchase_code', $input ) ) {
+	// Query for users based on the meta data
+	$user_query = new WP_User_Query(
+		array(
+			'meta_query' => array(
+				array(
+				'key' => '_cw_purchase_code',
+				'value' => strval($input),
+				'compare' => 'like',
+				)		
+			)
+		
+		)
+	);
+		
+	if ( 	$users = $user_query->get_results() ) {
 	
 		return true;
 	
@@ -97,6 +118,37 @@ function cw_validate_api( $cw_purcahse_code ){
 		return false;
 		
 	}
+	
+}
+
+/**
+ * Pulls all data from API and returns array.
+ * 
+ * @since 0.2
+ * @access public
+ * @param mixed $purchase_code
+ * @return array
+ */
+function cw_get_purchase_data($purchase_code){
+	
+	global $verifi;
+				
+	$market_class = $verifi->envato;
+		
+	$test_name = $verifi->username;
+		
+	$api_check = $market_class->verify_purchase( $test_name , $purchase_code);
+	
+	$meta = array(
+		"purchase_code" => $purchase_code,
+		"item_name" =>$api_check->item_name,
+		"item_id" => $api_check->item_id,
+		"created_at" => $api_check->created_at,
+		"buyer" => $api_check->buyer ,
+		"licence" =>   $api_check->licence
+	);
+	
+	return $meta;
 	
 }	
 ?>
