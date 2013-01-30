b3c1281e8227636f8ef02524262389dcf795d3e3
Chris Kelley <chris@organicbeemedia.com>
2013-01-29 22:14:20 -0600

    Updated shortcode with user password

--- includes/cw-verifi-shortcode.php
+++ includes/cw-verifi-shortcode.php
@@ -13,10 +13,9 @@
  *
  * cw_Verfi_Shortcode
  *	registration_form
- *	register_script
+ *	errors
  *	enqueue_scripts
  *	registration
- *	errors
  *	register_new_user
  *	display_message
  *
@@ -27,7 +26,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 if ( !class_exists( 'cw_Verfi_Shortcode' ) ) :
 
-class cw_Verfi_Shortcode {
+final class cw_Verfi_Shortcode {
 
 	private static $add_script;
 
@@ -38,8 +37,6 @@ class cw_Verfi_Shortcode {
 		add_shortcode('cw-verifi-registration', array( $this, 'registration_form'));
 		
 		add_action('init', array( $this, 'register_new_user' ));
-
-		add_action('wp_enqueue_scripts', array( $this , 'register_script'));
 		
 		add_action('wp_footer', array( $this , 'enqueue_scripts'));
 				
@@ -86,19 +83,6 @@ class cw_Verfi_Shortcode {
 	}
 	
 	/**
-	 * Register the css
-	 * 
-	 * @since 0.1
-	 * @access public
-	 * @return void
-	 */
-	function register_script() {
-	
-		wp_register_style('cw-verifi-css', trailingslashit( CWV_CSS ) . 'cw-verifi.css', null, '1.0');
-	
-	}
-
-	/**
 	 * Load Scripts and Styles.
 	 *
 	 * @since 0.1 
@@ -106,7 +90,7 @@ class cw_Verfi_Shortcode {
 	 * @return void
 	 */
 	function enqueue_scripts() {
-		
+				
 		if ( ! self::$add_script )
 		
 			return;
@@ -117,32 +101,28 @@ class cw_Verfi_Shortcode {
 		
 		wp_enqueue_style('thickbox.css', '/'.WPINC.'/js/thickbox/thickbox.css', null, '1.0');
 		
-		wp_enqueue_style( 'cw-verifi-css' );
+		wp_enqueue_script('cw-verifi-check',trailingslashit( CWV_JS) . 'cw-verifi-short-reg.js' );
+
+		wp_enqueue_style( 'cw-verifi-css', trailingslashit( CWV_CSS ) . 'cw-verifi.css', null, '1.0' );
 		
 		do_action( 'cw_verifi_shortcode_scripts' );
 		
 	}
 	
 	/**
-	 * wp_get_current_commenter function.
+	 * Load Errors
 	 * 
+	 * Thanks Pippin http://pippinsplugins.com
+	 * @since 0.1
 	 * @access public
-	 * @return array
+	 * @return mixed
 	 */
-	function get_ids() {
-
-		$username_id = 'cw_verifi_user_name';
-
-		$email_id = 'cw_verifi_user_email';
-
-		$purchase_id = 'cw_verifi_purchase_code';
-
-		$nonce_id = 'cw_verifi_nonce';
-		
-		$create_nonce = 'cw-verifi-nonce';
-
-		return apply_filters('cw_verifi_ids', compact('username_id' , 'email_id', 'purchase_id', 'nonce_id', 'create_nonce'));
-
+	function errors(){
+	
+		static $wp_error; // Will hold global variable safely
+    
+		return isset($wp_error) ? $wp_error : ($wp_error = new WP_Error(null, null, null));
+	
 	}
 	
 	/**
@@ -154,19 +134,26 @@ class cw_Verfi_Shortcode {
 	 * @todo filter
 	 */
 	function registration($args = array()){
-		
-		$id = $this->get_ids();
-		
+				
 		$fields = array(
-			'username' =>'<p>' . '<label for="' . esc_attr($id['username_id'] ) . '">'. __('Username', 'cw-verifi' ) .'<br />' .
-						 '<input name="' . esc_attr($id['username_id'] ) . '" id="' . esc_attr($id['username_id'] ) . '" class="required" type="text" placeholder="' . __('Please enter a unique username', 'cw-verifi') . '"/></label></p>',
+			'username' =>'<p>' . '<label for="cw_verifi_user_name">'. __('Username', 'cw-verifi' ) .'<br />' .
+						 '<input name="cw_verifi_user_name" id="cw_verifi_user_name" class="required" type="text" placeholder="' . __('Please enter a unique username', 'cw-verifi') . '" size="25" /></label></p>',
 			
-			'user_email' =>'<p>' . '<label for="' . esc_attr($id['email_id'] ) . '">'. __('Email', 'cw-verifi') . '<br />' .
-						   '<input name="' . esc_attr($id['email_id'] ) . '" id="' . esc_attr($id['email_id'] ) . '" class="required" type="text" placeholder="'. __('Please enter your email', 'cw-verifi').'"/></label></p>',
 			
-			'purchase_code' =>'<p>' . '<label for="' . esc_attr($id['purchase_id'] ) . '">'. __('Purchase Code', 'cw-verifi') . '<span>&nbsp;(<a class="thickbox" href="' . trailingslashit( CWV_IMAGES ) . 'purchasecode.jpg' .'">whats this</a>)</span>' .
-							  '<br /><input name="' . esc_attr($id['purchase_id'] ) . '" id="' . esc_attr($id['purchase_id'] ) . '" class="required" type="text" placeholder="'. __('Please enter your purchase code', 'cw-verifi') .'"/></label></p>'
-		
+			
+			'user_email' =>'<p>' . '<label for="cw_verifi_user_email">'. __('Email', 'cw-verifi') . '<br />' .
+						   '<input name="cw_verifi_user_email" id="cw_verifi_user_email" class="required" type="text" placeholder="'. __('Please enter your email', 'cw-verifi').'" size="25" /></label></p>',
+			
+			
+			'user_pass' =>'<p>' . '<label for="cw_verifi_user_pass">'. __('Password', 'cw-verifi') . '<br />' .
+						   '<input name="cw_verifi_user_pass" id="cw_verifi_user_pass" class="required" type="password" placeholder="******" size="25" /></label></p>',		  
+		    
+		    'confirm_pass' =>'<p>' . '<label for="cw_verifi_confirm_pass">'. __('Confirm Password', 'cw-verifi') . '<br />' .
+						   '<input name="cw_verifi_confirm_pass" id="cw_verifi_confirm_pass" class="required" type="password" placeholder="******" size="25" /></label></p>',
+			
+			'purchase_code' =>'<p>' . '<label for="cw_verifi_purchase_code">'. __('Purchase Code', 'cw-verifi') . '<span>&nbsp;(<a class="thickbox" href="' . trailingslashit( CWV_IMAGES ) . 'purchasecode.jpg' .'">whats this</a>)</span>' .
+							  '<br /><input name="cw_verifi_purchase_code" id="cw_verifi_purchase_code" class="required" type="text" placeholder="'. __('Please enter your purchase code', 'cw-verifi') .'" size="25" /></label></p>'
+
 		);
 		
 		$defaults = array(
@@ -187,7 +174,7 @@ class cw_Verfi_Shortcode {
 		
 		<form id="<?php echo $args['form_id']; ?>" class="verifi-form" action="" method="POST">
 		
-				<?php do_action('cw_verfifi_before_form_fields'); ?>
+				<?php do_action('cw_verifi_before_form_fields'); ?>
 			
 				<?php
 			
@@ -197,35 +184,21 @@ class cw_Verfi_Shortcode {
 							
 				?>
 			
-				<?php do_action('cw_verfifi_after_form_fields'); ?>
-
+				<?php do_action('cw_verifi_after_form_fields'); ?>
+				
+				<p>Password must be at least 6 characters</p>
+				
 				<p>
 				
-					<input type="hidden" name="<?php esc_attr_e($id['nonce_id']);  ?>" value="<?php echo wp_create_nonce($id['create_nonce']); ?>"/>
+					<input type="hidden" name="cw_verifi_nonce" value="<?php echo wp_create_nonce('cw-verifi-nonce'); ?>"/>
 				
-					<input type="submit" id="<?php  esc_attr_e($id['nonce_id'] ); ?>" value="<?php _e('Register an Account', 'cw-verifi'); ?>"/>
+					<input type="submit" id="cw_verifi_nonce" value="<?php _e('Register an Account', 'cw-verifi'); ?>"/>
 				
 				</p>
 					
 		</form>
 		
 		<?php
-	}
-	
-	/**
-	 * Load Errors
-	 * 
-	 * Thanks Pippin http://pippinsplugins.com
-	 * @since 0.1
-	 * @access public
-	 * @return mixed
-	 */
-	function errors(){
-	
-		static $wp_error; // Will hold global variable safely
-    
-		return isset($wp_error) ? $wp_error : ($wp_error = new WP_Error(null, null, null));
-	
 	}
 	
 	/**
@@ -238,17 +211,22 @@ class cw_Verfi_Shortcode {
 	 * @return mixed
 	 */
 	function register_new_user() {
-
-		$id = $this->get_ids();
 	
-  		if (isset( $_POST[ $id['username_id']] ) &&  wp_verify_nonce($_POST[$id['nonce_id']], $id['create_nonce'])) {
+		static $wp_error; // Will hold global variable safely
+    
+		isset($wp_error) ? $wp_error : ($wp_error = new WP_Error(null, null, null));
 			
-			$sanitized_user_login = sanitize_user($_POST[$id['username_id']]);	
+  		if (isset( $_POST['cw_verifi_user_name'] ) &&  wp_verify_nonce($_POST['cw_verifi_nonce'], 'cw-verifi-nonce')) {
 			
-			$user_email		= $_POST[$id['email_id']];
+			$sanitized_user_login = sanitize_user($_POST['cw_verifi_user_name']);	
 			
-			$purchase_code = $_POST[$id['purchase_id']];
+			$user_email	= $_POST['cw_verifi_user_email'];
 			
+			$user_pass =$_POST['cw_verifi_user_pass'];
+			
+			$confirm_pass =$_POST['cw_verifi_confirm_pass'] ;
+			
+			$purchase_code = $_POST['cw_verifi_purchase_code'];
 		
 			if(username_exists($sanitized_user_login)) {
 				
@@ -279,6 +257,26 @@ class cw_Verfi_Shortcode {
 				$this->errors()->add('email_used', __('<strong>ERROR</strong>:Email already registered', 'cw-verifi'));
 			
 			}
+			
+			if ( $user_pass == '' ) {
+				
+				$this->errors()->add( 'empty_pass', __( '<strong>ERROR</strong>: Please enter a password.' ) );
+		
+			} elseif ( $user_pass != $confirm_pass ) {
+				
+				$this->errors()->add( 'pass_match', __( '<strong>ERROR</strong>: Your passwords dont match!' ) );
+			
+			} elseif( strlen($user_pass) < 6 ){ 
+		
+				$this->errors()->add( 'short_pass', __( '<strong>ERROR</strong>: Your Password is too short.' ) );
+
+			}	
+			
+			if (  $confirm_pass == '' ){
+			
+				$this->errors()->add( 'empty_confirm', __( '<strong>ERROR</strong>: Please confirm your password' ) );
+			
+			}
 		
 			if ( $purchase_code == '' ) {
 				
@@ -304,11 +302,7 @@ class cw_Verfi_Shortcode {
 			return $errors;
 			
 			} 
-			
-			$this->success = apply_filters( 'cw_verifi_success_message', __('A Password will be sent to your email', 'cw-verifi') ); 
-
-			$user_pass = wp_generate_password( 12, false);
-			
+						
 			$user_id = wp_create_user( $sanitized_user_login, $user_pass, $user_email );
 			
 			if ( ! $user_id ) {
@@ -318,19 +312,34 @@ class cw_Verfi_Shortcode {
 				return $errors;
 			
 			}
-
-			update_user_option( $user_id, 'default_password_nag', true, true ); 
-	
-			$meta = cw_get_purchase_data($purchase_code);
 			
+			$meta = cw_get_purchase_data($purchase_code);			
+			
+
+			//Add all meta to db
 			update_user_meta( $user_id, '_cw_purchase_code' , $meta );
+				
+			wp_new_user_notification( $user_id );	
 			
-			wp_new_user_notification( $user_id, $user_pass );
+			//Lets set a cookie for 10 minutes so we can display cool messages 
+			if(!isset($_COOKIE['cw_verifi_new_user'])){
 			
+				setcookie('cw_verifi_new_user', 1,  time() + (60 * 10), COOKIEPATH, COOKIE_DOMAIN, false );
 			
-			return $user_id;
+			}	
+			
+			$credentials = array();
+			$credentials['user_login'] = $sanitized_user_login;
+			$credentials['user_password'] = $user_pass;
+			$credentials['remember'] = true;
+		
+			wp_signon( $credentials );
+			
+			$redirect = apply_filters('cw_short_redirect', home_url());
+			
+			wp_safe_redirect($redirect); exit;
 
-		}
+			}
 	
 	}
 	
@@ -358,9 +367,6 @@ class cw_Verfi_Shortcode {
 		       
 		   echo '</div>';
 		   
-		}elseif( $this->success != ''){
-			
-			echo '<div class="cw-verifi-success"><span>' . $this->success . '</span></div>';
 		}
 	
 	}
