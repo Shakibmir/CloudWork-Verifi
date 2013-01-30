3c5b99c29a12cbc2b860ed6144049945318f3de4
Chris Kelley <chris@organicbeemedia.com>
2013-01-29 22:15:19 -0600

    moved login form functions to seperate file

--- cw-verifi.php
+++ cw-verifi.php
@@ -3,7 +3,7 @@
 Plugin Name: CloudWork Verifi
 Plugin URI: http://cloudworkthemes.com
 Description: Uses Envato API to verify purchase at registration, prevents duplicate purchase codes
-Version: 0.2.1
+Version: 0.3
 Author: Chris Kelley <chris@organicbeemedia.com>
 Author URI: http://cloudworkthemes.com
 License: GPLv2
@@ -17,10 +17,6 @@ License: GPLv2
 *	includes
 *	globals
 *	load_textdomain
-*	login_scripts
-*	purchase_code_field
-*	register_field
-*	check_field
 *
 */
 
@@ -49,14 +45,6 @@ final class cw_Verifi{
 	 * @return void
 	 */
 	private function __construct(){
-	
-		add_action('login_enqueue_scripts', array( $this, 'login_scripts' ));
-	
-		add_action('register_form', array( $this, 'purchase_code_field' ));
-
-		add_action('user_register', array( $this, 'register_field' ));
-
-		add_action('register_post', array( $this, 'check_fields' ),10,3);
 
 		add_action('admin_notices', array( $this, 'admin_notice' ));
 
@@ -160,7 +148,12 @@ final class cw_Verifi{
 		
 		require_once trailingslashit( CWV_INCLUDES ) . 'cw-verifi-functions.php';
 
+		require_once trailingslashit( CWV_INCLUDES ) . 'cw-verifi-login.php';
+
 		require_once trailingslashit( CWV_INCLUDES ) . 'cw-verifi-shortcode.php';
+		
+		require_once trailingslashit( CWV_INCLUDES ) . 'cw-verifi-usershort.php';
+
 
 		if( is_admin()){
 	
@@ -205,35 +198,45 @@ final class cw_Verifi{
 	}
 	
 	/**
-	 * Loads scripts needed for thickbox, used to display purchase code image
+	 * Creates Error notices if options arent set.
 	 * 
 	 * @access public
+	 * @param mixed $message
 	 * @return void
 	 */
-	function login_scripts(){
-			
-		wp_enqueue_script('jquery');
-		
-		wp_enqueue_script('thickbox', null,  array('jquery'));
-		
-		wp_enqueue_style('thickbox.css', '/'.WPINC.'/js/thickbox/thickbox.css', null, '1.0');
+	function admin_notice(){
 	
+		//Wrap notices with link to options page
+		$url = admin_url( 'options-general.php?page=cw-verifi-options' );
+	
+		//Dont display if user cant manage options
+		if ( current_user_can( 'manage_options' ) ){
+			
+			if( $this->username == ''){
+	
+			echo '<div class="error"><a href="'. $url .'"><p>' . __('Please enter your Envato username', 'cw-verifi') . '</p></a></div>';
+			
+			}
+			
+			if( $this->apikey == ''){
+	
+			echo '<div class="error"><a href="'. $url .'"><p>' . __('Please enter your Envato API Key', 'cw-verifi') . '</p></a></div>';
+			
+			}
+		
+		}
+		
 	}
 	
-	
-	/**
-	 * Creates new field for registration form
-	 * 
-	 * @since 0.1
-	 * @access public
-	 * @return string
-	 */
-	function purchase_code_field(){
-	
-	?>
 
-		<p>
-			<label for="cw_purchase_code"><?php  _e('Purchase Code', 'cw-verifi')?><span>&nbsp;(<a class="thickbox" href="<?php echo  trailingslashit( CWV_IMAGES ) . 'purchasecode.jpg'; ?>">what's this</a>)</span><br />
+}//Ends Class
+
+endif; //end if 
+
+//Jedi Mind Tricks
+$verifi = cw_Verifi::instance();
+//May the force be with you
+?>ngslashit( CWV_IMAGES ) . 'purchasecode.jpg'; ?>">what's this</a>)</span><br />
 	
 			<input type="text" name="cw_purchase_code" id="cw_purchase_code" class="input" value="<?php isset( $_POST['cw_purchase_code'] ) ?>" size="20"  /></label>
 
